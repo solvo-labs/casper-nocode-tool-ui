6d1aff2d84d18483e7676620657948b83333e20f
ayseguleren <ayse.eren@windowslive.com>
2023-06-07 18:07:55 +0300

    creation enums

---
+++ src/utils/enum.ts
@@ -0,0 +1,10 @@
+export enum APP_NAME {
+  CASPER = "CASPER NOCODE TOOL",
+}
+export enum PAGES_NAME {
+  STAKING = "STAKING",
+  DAO = "DAO",
+  NFT = "NFT",
+  TOKEN = "TOKEN",
+  TOKEN_MINT = "Token Mint",
+}
