3aa07f4f0cf73325a9ca154324f3770ce610f232
ayseguleren <ayse.eren@windowslive.com>
2023-06-07 16:07:51 +0300

    base react-router-dom creation

---
+++ src/router/ProtectedRoute.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { Card, Grid, Theme } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
+import GoogleFontLoader from "react-google-font-loader";
+import SideBar from "../components/SideBar";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    padding: "2rem",
+    overflow: "scrool",
+    [theme.breakpoints.down("md")]: {
+      padding: "1rem",
+    },
+  },
+}));
+
+export const ProtectedRoute = () => {
+  const classes = useStyles();
+
+  return (
+    <div
+      style={{
+        backgroundColor: "#E7EBF1",
+      }}
+      className={classes.container}
+    >
+      <Grid container spacing={2}>
+        <Grid item lg={2} md={3}>
+          <SideBar />
+        </Grid>
+        <Grid item lg={10} md={9} xs={12}>
+          <Grid container direction={"column"} spacing={2}>
+            <Grid item>{/* <DrawerAppBar /> */}</Grid>
+            <Grid item sx={{ width: "100%" }}>
+              <Card
+                sx={{
+                  borderRadius: "40px",
+                }}
+              >
+                <GoogleFontLoader fonts={[{ font: "Inter", weights: [700, "700i", 500, "500i"] }]} subsets={["cyrillic-ext", "sans-serif", "greek"]} />
+                <div>
+                  <Outlet />
+                </div>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
---
+++ src/router/Router.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { ProtectedRoute } from "./ProtectedRoute";
+import Login from "../Pages/Login";
+
+const Router: React.FC = () => {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route element={<ProtectedRoute />}></Route>
+          <Route path="/login" index element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default Router;
