cc373c9219855cc14423b6101a812a239df30911
afarukcali <afarukcali@gmail.com>
2023-06-15 12:16:45 +0300

    app.tsx revert.

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,9 @@ import React, { useEffect } from "react";
 import Router from "./router/Router";
 import { ThemeProvider } from "@mui/styles";
 import { createTheme } from "@mui/material";
-import { fetchNamedKeys } from "./utils/api";
 
 const App = () => {
   const theme = createTheme();
-
-  useEffect(() => {
-    const init = async () => {
-      const x = await fetchNamedKeys("5e542e3bfacb53152a07322519eedd6f6cad1689508d588051603459b4b12590");
-
-      console.log(x);
-    };
-
-    init();
-  }, []);
 
   return (
     <ThemeProvider theme={theme}>
