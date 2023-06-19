68031c6e0a8d628c40f3dea816077819ff6ae07d
ayseguleren <ayse.eren@windowslive.com>
2023-06-19 14:52:58 +0300

    Main: demo for CustomSelect

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CustomSelect } from "../components/CustomSelect";
+import { MenuItem } from "@mui/material";
+
+enum selectValues {
+  MENU_1 = "menu 1",
+  MENU_2 = "menu 2",
+  MENU_3 = "menu 3",
+  MENU_4 = "menu 4",
+}
 
 const Main: React.FC = () => {
-  return <>Main Page</>;
+  const [value, setValue] = useState<selectValues>(selectValues.MENU_1);
+
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
+
+  return (
+    <div
+      style={{
+        height: "calc(100vh - 8rem)",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CustomSelect
+        id="select"
+        onChange={(event: any) => {
+          setValue(event.target.value);
+        }}
+        value={value}
+      >
+        <MenuItem value={selectValues.MENU_1}>{selectValues.MENU_1}</MenuItem>
+        <MenuItem value={selectValues.MENU_2}>{selectValues.MENU_2}</MenuItem>
+        <MenuItem value={selectValues.MENU_3}>{selectValues.MENU_3}</MenuItem>
+        <MenuItem value={selectValues.MENU_4}>{selectValues.MENU_4}</MenuItem>
+      </CustomSelect>
+    </div>
+  );
 };
 
 export default Main;
