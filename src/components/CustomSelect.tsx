fe92d8256920d461a9f3f52032dda1c872b179b4
ayseguleren <ayse.eren@windowslive.com>
2023-06-19 14:52:44 +0300

    creation CustomSelect

---
+++ src/components/CustomSelect.tsx
@@ -0,0 +1,59 @@
+import React, { Children, cloneElement } from "react";
+import { Select, Theme } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  select: {
+    color: "white !important",
+    boxShadow: "0 0 10px 0 rgba(0,0,0,0.1)",
+    backgroundColor: "#FF3341 !important",
+    width: "10rem !important",
+  },
+  selectedMenuItem: {
+    backgroundColor: "#FF3341 !important",
+    color: "white !important",
+  },
+}));
+
+type Props = {
+  id: string;
+  onChange: any;
+  value?: any;
+  label?: string;
+  children: any;
+};
+
+export const CustomSelect: React.FC<Props> = ({ onChange, value, children, id, label }) => {
+  const classes = useStyles();
+
+  return (
+    <Select
+      classes={{ select: classes.select }}
+      sx={{
+        "& .css-jedpe8-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.MuiSelect-select": {
+          height: "27px",
+          borderRadius: "1rem",
+        },
+        "& .MuiOutlinedInput-notchedOutline": {
+          borderRadius: "1rem",
+          //TODO: for just clicked
+          //border: "0.1rem solid #FF0011 !important",
+          border: "0.1rem solid #0F1429 !important",
+        },
+      }}
+      onChange={onChange}
+      value={value}
+      variant="outlined"
+      size="small"
+      fullWidth
+      id={id}
+      label={label}
+    >
+      {Children.map(children, (child) => {
+        return cloneElement(child, {
+          classes: { root: child.props.value === value ? classes.selectedMenuItem : "" },
+        });
+      })}
+    </Select>
+  );
+};
