8895ad8224882cfeca438584f57bb9209e59d565
afarukcali <afarukcali@gmail.com>
2023-06-20 11:51:05 +0300

    build

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,21 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { CustomSelect } from "../components/CustomSelect";
-import { MenuItem } from "@mui/material";
-
-enum selectValues {
-  MENU_1 = "menu 1",
-  MENU_2 = "menu 2",
-  MENU_3 = "menu 3",
-  MENU_4 = "menu 4",
-}
+import React from "react";
 
 const Main: React.FC = () => {
-  const [value, setValue] = useState<selectValues>(selectValues.MENU_1);
-
-  useEffect(() => {
-    console.log(value);
-  }, [value]);
-
   return (
     <div
       style={{
