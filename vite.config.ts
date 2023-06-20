ee33ad644474e6782d08a40e24fc5d8186e9d482
afarukcali <afarukcali@gmail.com>
2023-06-20 15:28:46 +0300

    wasm plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react()],
+    plugins: [react(), wasm()],
     base: "/",
   };
 
