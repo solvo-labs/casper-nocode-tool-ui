be534e67c5ed3e3df7424c4aedcd3eb530505c28
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-07-14 18:24:06 +0300

    MyCollection: page created

--- src/Pages/NFT/MyCollections.tsx
+++ src/Pages/NFT/MyCollections.tsx
@@ -1,9 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { useOutletContext } from "react-router-dom";
+import { useNavigate, useOutletContext } from "react-router-dom";
 // @ts-ignore
 import { CLPublicKey } from "casper-js-sdk";
 import { fetchCep78NamedKeys, getNftCollection } from "../../utils/api";
-import { CircularProgress } from "@mui/material";
+import {
+  Card,
+  CircularProgress,
+  Grid,
+  Stack,
+  Theme,
+  Typography,
+} from "@mui/material";
+import { NftCard } from "../../components/NftCard";
+import { makeStyles } from "@mui/styles";
+import { CustomButton, CustomButtonText } from "../../components/CustomButton";
+import { CreateCollectionCard } from "../../components/CreateCollectionCard";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  titleContainer: {
+    [theme.breakpoints.down("md")]: {
+      paddingLeft: "1rem",
+      paddingRight: "1rem",
+    },
+  },
+  container: {
+    maxWidth: "80vw",
+    minWidth: "80vw",
+    marginTop: "4rem",
+    // backgroundColor: "darkgray",
+    [theme.breakpoints.down("md")]: {
+      maxWidth: "95vw",
+      marginTop: "4rem",
+    },
+  },
+  title: {
+    position: "relative",
+    borderBottom: "1px solid #FF0011 !important",
+  },
+}));
 
 export const MyCollections = () => {
   const [publicKey] = useOutletContext<[publickey: string]>();
@@ -11,6 +45,8 @@ export const MyCollections = () => {
 
   // @to-do add collection model
   const [collections, setCollections] = useState<any>([]);
+  const classes = useStyles();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const init = async () => {
@@ -29,21 +65,77 @@ export const MyCollections = () => {
     init();
   }, []);
 
-  if (loading) {
-    return (
-      <div
-        style={{
-          height: "calc(100vh - 8rem)",
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <CircularProgress />
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div
+  //       style={{
+  //         height: "calc(100vh - 8rem)",
+  //         width: "100%",
+  //         display: "flex",
+  //         justifyContent: "center",
+  //         alignItems: "center",
+  //       }}
+  //     >
+  //       <CircularProgress />
+  //     </div>
+  //   );
+  // }
 
-  return <span>My Collections Page</span>;
+  return (
+    <div>
+      <Grid
+        container
+        className={classes.titleContainer}
+        direction={"row"}
+        justifyContent={"space-between"}
+      >
+        <Typography className={classes.title} variant="h4">
+          My Collections
+        </Typography>
+        <Stack direction={"row"} spacing={2}>
+          <CustomButtonText
+            disabled={false}
+            label="My NFTs"
+            onClick={() => {
+              navigate("/my-nfts");
+            }}
+          ></CustomButtonText>
+          <CustomButtonText
+            disabled={false}
+            label="My Collections"
+            onClick={() => {
+              navigate("/my-collections");
+            }}
+          ></CustomButtonText>
+          <CustomButton
+            disabled={false}
+            label="Mint NFT"
+            onClick={() => {
+              navigate("/create-nft");
+            }}
+          ></CustomButton>
+        </Stack>
+      </Grid>
+      <Grid container className={classes.container}>
+        <Grid container width={"100%"} justifyContent={"flex-start"}>
+          <Grid item lg={4} md={4} sm={6} xs={6}>
+            <CreateCollectionCard
+              onClick={() => {
+                navigate("/create-collection");
+              }}
+            />
+          </Grid>
+          <Grid item lg={4} md={4} sm={6} xs={6}>
+            <NftCard></NftCard>
+          </Grid>
+          <Grid item lg={4} md={4} sm={6} xs={6}>
+            <NftCard></NftCard>
+          </Grid>
+          <Grid item lg={4} md={4} sm={6} xs={6}>
+            <NftCard></NftCard>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
