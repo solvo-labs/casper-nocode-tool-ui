cf16adb6ba640aefdcdea527e01792f9680f4e8b
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-08-18 18:06:54 +0300

    fix card actionarea

--- src/components/CollectionCard.tsx
+++ src/components/CollectionCard.tsx
@@ -31,27 +31,29 @@ export const CollectionCard: React.FC<Props> = ({
   title,
   symbol,
   contractHash,
-  onClick
+  onClick,
 }) => {
   const classes = useStyles();
   return (
-    <Card className={classes.card} onClick={onClick}>
-      <CardMedia
-        component="img"
-        height="200"
-        image="https://w0.peakpx.com/wallpaper/237/346/HD-wallpaper-gt-r-nissan-japanese-car-cartoon.jpg"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
-        </Typography>
-        <Typography variant="h6">{symbol}</Typography>
-        <Typography variant="body2" color="text.secondary">
-          {contractHash.slice(0, 20)}
-        </Typography>
-      </CardContent>
-    </Card>
+    <CardActionArea>
+      <Card className={classes.card} onClick={onClick}>
+        <CardMedia
+          component="img"
+          height="200"
+          image="https://w0.peakpx.com/wallpaper/237/346/HD-wallpaper-gt-r-nissan-japanese-car-cartoon.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="h6">{symbol}</Typography>
+          <Typography variant="body2" color="text.secondary">
+            {contractHash.slice(0, 20)}
+          </Typography>
+        </CardContent>
+      </Card>
+    </CardActionArea>
   );
 };
 
