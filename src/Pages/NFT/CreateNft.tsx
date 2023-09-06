fe580972be680ad9f8346c88a28e4c5c005aa54d
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-09-06 19:27:45 +0300

    CreateNFT: imageUpload component added

--- src/Pages/NFT/CreateNft.tsx
+++ src/Pages/NFT/CreateNft.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 // @ts-ignore
 import { Contracts, RuntimeArgs, CLPublicKey, DeployUtil, CLValueBuilder } from "casper-js-sdk";
 import { useOutletContext } from "react-router-dom";
@@ -10,6 +10,8 @@ import { Grid, Stack, Theme, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { CustomInput } from "../../components/CustomInput";
 import { CustomButton } from "../../components/CustomButton";
+import ImageUpload from "../../components/ImageUpload";
+import { NFTStorage } from "nft.storage";
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
@@ -46,68 +48,127 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 export const CreateNft = () => {
-  const [publicKey, provider] = useOutletContext<[publickey: string, provider: any]>();
+  const [publicKey, provider] =
+    useOutletContext<[publickey: string, provider: any]>();
   const classes = useStyles();
   const [nftData, setNftData] = useState<NFT>({
     contractHash: "",
     tokenMetaData: {
       name: "",
       description: "",
+      imageURL: "",
     },
   });
 
-  let hashLength = nftData.contractHash.length;
-  let hashCheck = nftData.contractHash.startsWith("hash-");
-  const disable = !(nftData.contractHash && nftData.tokenMetaData && hashCheck && hashLength == 69);
+  const [file, setFile] = useState<any>();
+  const [fileLoading, setFileLoading] = useState<boolean>(false);
+
+  const disable = useMemo(() => {
+    let hashLength = nftData.contractHash.length;
+    let hashCheck = nftData.contractHash.startsWith("hash-");
+    const disable = !(
+      nftData.contractHash &&
+      nftData.tokenMetaData &&
+      hashCheck &&
+      hashLength == 69 &&
+      !fileLoading
+    );
+    return disable;
+  }, [nftData, fileLoading]);
 
   const createNft = async () => {
-    const contract = new Contracts.Contract();
-
-    // "hash-5480fd53270a9768dc9c37ac41921a583d7f19095479f89552cda74185cca66c"
-    contract.setContractHash(nftData.contractHash);
-    const ownerPublicKey = CLPublicKey.fromHex(publicKey);
-
-    const args = RuntimeArgs.fromMap({
-      token_owner: CLValueBuilder.key(ownerPublicKey),
-      token_meta_data: CLValueBuilder.string(
-        nftData.tokenMetaData
-        // JSON.stringify({
-        //   name: "Kaaacca2",
-        //   description: "hello",
-        //   asset:
-        //     "https://maritime.sealstorage.io/ipfs/bafkreia2xhh4rh5tmlpy5srlzzezvm2nabm7rcvuhtjexhj5hh42yidiku",
-        // }
-        // )
-      ),
-    });
-
-    const deploy = contract.callEntrypoint("mint", args, ownerPublicKey, "casper-test", "2000000000");
-
-    const deployJson = DeployUtil.deployToJson(deploy);
-
     try {
-      const sign = await provider.sign(JSON.stringify(deployJson), publicKey);
+      const contract = new Contracts.Contract();
 
-      // setActionLoader(true);
+      contract.setContractHash(nftData.contractHash);
+      console.log(nftData.tokenMetaData);
+      const ownerPublicKey = CLPublicKey.fromHex(publicKey);
 
-      let signedDeploy = DeployUtil.setSignature(deploy, sign.signature, ownerPublicKey);
-
-      signedDeploy = DeployUtil.validateDeploy(signedDeploy);
-
-      const deployData = DeployUtil.deployToJson(signedDeploy.val);
-
-      const response = await axios.post(SERVER_API + "deploy", deployData, {
-        headers: { "Content-Type": "application/json" },
+      const args = RuntimeArgs.fromMap({
+        token_owner: CLValueBuilder.key(ownerPublicKey),
+        token_meta_data: CLValueBuilder.string(
+          JSON.stringify(nftData.tokenMetaData)
+        ),
       });
-      toastr.success(response.data, "Mint completed successfully.");
-      window.open("https://testnet.cspr.live/deploy/" + response.data, "_blank");
 
-      // navigate("/my-tokens");
-      // setActionLoader(false);
-    } catch (error: any) {
-      alert(error.message);
+      const deploy = contract.callEntrypoint(
+        "mint",
+        args,
+        ownerPublicKey,
+        "casper-test",
+        "2000000000"
+      );
+
+      const deployJson = DeployUtil.deployToJson(deploy);
+
+      try {
+        const sign = await provider.sign(JSON.stringify(deployJson), publicKey);
+
+        // setActionLoader(true);
+
+        let signedDeploy = DeployUtil.setSignature(
+          deploy,
+          sign.signature,
+          ownerPublicKey
+        );
+
+        signedDeploy = DeployUtil.validateDeploy(signedDeploy);
+
+        const deployData = DeployUtil.deployToJson(signedDeploy.val);
+
+        const response = await axios.post(SERVER_API + "deploy", deployData, {
+          headers: { "Content-Type": "application/json" },
+        });
+        toastr.success(response.data, "Mint completed successfully.");
+        window.open(
+          "https://testnet.cspr.live/deploy/" + response.data,
+          "_blank"
+        );
+
+        // navigate("/my-tokens");
+        // setActionLoader(false);
+      } catch (error: any) {
+        alert(error.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toastr.error("error");
     }
   };
+
+  useEffect(() => {
+    const storeImage = async () => {
+      if (file) {
+        setFileLoading(true);
+        const storage = new NFTStorage({
+          token: import.meta.env.VITE_NFT_STORAGE_API_KEY,
+        });
+        const fileCid = await storage.storeBlob(new Blob([file]));
+
+        const fileUrl = "https://ipfs.io/ipfs/" + fileCid;
+
+        const obj = {
+          image: fileUrl,
+        };
+
+        const metadata = new Blob([JSON.stringify(obj)], {
+          type: "application/json",
+        });
+        const metadataCid = await storage.storeBlob(metadata);
+        const metadataUrl = "https://ipfs.io/ipfs/" + metadataCid;
+        setNftData({
+          ...nftData,
+          tokenMetaData: {
+            ...nftData.tokenMetaData,
+            imageURL: metadataUrl,
+          },
+        });
+        setFileLoading(false);
+      }
+    };
+
+    storeImage();
+  }, [file]);
 
   return (
     <div
@@ -125,7 +186,12 @@ export const CreateNft = () => {
             </Typography>
           </Grid>
           <Grid container className={classes.gridContainer}>
-            <Stack spacing={4} direction={"column"} marginTop={4} className={classes.stackContainer}>
+            <Stack
+              spacing={4}
+              direction={"column"}
+              marginTop={4}
+              className={classes.stackContainer}
+            >
               <CustomInput
                 placeholder="Collection Address"
                 label="Collection Address"
@@ -141,9 +207,17 @@ export const CreateNft = () => {
                 value={nftData.contractHash}
               ></CustomInput>
               {/* //TODO nft metadata input */}
-              <Typography sx={{ borderBottom: "1px solid #FF0011 !important" }} variant="button">
+              <Typography
+                sx={{ borderBottom: "1px solid #FF0011 !important" }}
+                variant="button"
+              >
                 Metadata
               </Typography>
+              <ImageUpload
+                file={file}
+                loading={fileLoading}
+                setFile={(data) => setFile(data)}
+              ></ImageUpload>
               <CustomInput
                 placeholder="Metadata Name"
                 label="Metadata Name"
@@ -180,7 +254,11 @@ export const CreateNft = () => {
               ></CustomInput>
 
               <Grid paddingTop={2} container justifyContent={"center"}>
-                <CustomButton onClick={createNft} disabled={disable} label="Create NFT" />
+                <CustomButton
+                  onClick={createNft}
+                  disabled={disable}
+                  label="Create NFT"
+                />
               </Grid>
             </Stack>
           </Grid>
