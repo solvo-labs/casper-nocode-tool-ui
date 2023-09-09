157cf1af6f44526bee9b0d5fbe2ad83c5dc3e409
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-09-09 18:28:00 +0300

    enum: marketplace pages

--- src/utils/enum.ts
+++ src/utils/enum.ts
@@ -8,6 +8,7 @@ export enum PAGES_NAME {
   NFT = "NFT",
   TOKEN = "TOKEN",
   TOKENOMICS = "TOKENOMICS",
+  MARKETPLACE = "MARKETPLACE"
 }
 
 export enum TOKEN_PAGE {
@@ -30,6 +31,11 @@ export enum NFT_PAGE {
   MY_COLLECTIONS = "My Collections",
   CREATE_NFT = "Create NFT",
   CREATE_COLLECTION = "Create Collection",
+}
+
+export enum MARKETPLACE_PAGE {
+  MARKETPLACE = "Marketplace",
+  CREATE_MARKETPLACE = "Create Marketplace",
 }
 
 export enum WALLETS_NAME {
