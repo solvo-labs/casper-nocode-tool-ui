1658854aebbb3886cc42be8aeb5c34944471b0dc
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-09-09 18:28:35 +0300

    CreateMarketplace: page created

---
+++ src/Pages/Marketplace/CreateMarketplace.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { CustomButton } from "../../components/CustomButton";
+
+const CreateMarketplace = () => {
+  return (
+    <CustomButton
+      disabled={false}
+      label="Create Marketplace"
+      onClick={() => {}}
+    ></CustomButton>
+  );
+};
+
+export default CreateMarketplace;
