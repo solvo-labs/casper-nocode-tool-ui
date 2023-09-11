f738a87c78f56fa07f3b67e5a7335bddc6f11258
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-09-11 05:43:24 +0300

    created ListMarketplace pages

---
+++ src/Pages/Marketplace/ListMarketplace.tsx
@@ -0,0 +1,62 @@
+import { CircularProgress, Grid, Stack } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { MarketplaceCard } from "../../components/MarketplaceCard";
+import { CLPublicKey } from "casper-js-sdk";
+import { fetchMarketplaceNamedKeys, getNftCollection } from "../../utils/api";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { Marketplace } from "../../utils/types";
+
+const ListMarketplace = () => {
+  const [publicKey] = useOutletContext<[publickey: string]>();
+  const [marketplace, setMarketplace] = useState<Marketplace[]>([]);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const init = async () => {
+      const ownerPublicKey = CLPublicKey.fromHex(publicKey);
+
+      const data = await fetchMarketplaceNamedKeys(
+        ownerPublicKey.toAccountHashStr()
+      );
+      
+      setLoading(false);
+      setMarketplace(data);
+      console.log(data);
+    };
+
+    init();
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          height: "50vh",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  return (
+    <Grid>
+      <Stack>
+        {marketplace.map((e: any) => (
+          <MarketplaceCard
+            hash={e.key.slice(0,20)}
+            name={e.name}
+            onClick={() => navigate("/marketplace/" + e.key)}
+          ></MarketplaceCard>
+        ))}
+      </Stack>
+    </Grid>
+  );
+};
+
+export default ListMarketplace;
