58933fecd8ee663ab6067ecc5605929cbd1096be
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-10-25 17:18:28 +0300

    used creator component

--- src/Pages/Marketplace/ListMarketplace.tsx
+++ src/Pages/Marketplace/ListMarketplace.tsx
@@ -6,6 +6,8 @@ import { CLPublicKey } from "casper-js-sdk";
 import { fetchMarketplaceNamedKeys } from "../../utils/api";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { Marketplace } from "../../utils/types";
+import CreatorRouter from "../../components/CreatorRouter";
+import { DONT_HAVE_ANYTHING } from "../../utils/enum";
 
 const ListMarketplace = () => {
   const [publicKey] = useOutletContext<[publickey: string]>();
@@ -46,13 +48,17 @@ const ListMarketplace = () => {
   return (
     <Grid>
       <Stack>
-        {marketplaces.length <= 0 && <Typography marginTop={"16rem"}>Could not find the marketplace to be displayed.</Typography>}
+        {marketplaces.length <= 0 && (
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <CreatorRouter explain={DONT_HAVE_ANYTHING.MARKETPLACE} handleOnClick={() => navigate("/create-marketplace")}></CreatorRouter>
+          </div>
+        )}
         {marketplaces.length > 0 && (
           <div>
             <Typography variant="h5">Active MarketPlace List</Typography>
             <Divider sx={{ backgroundColor: "red", marginBottom: " 1rem !important" }}></Divider>
-            {marketplaces.map((e: Marketplace) => (
-              <MarketplaceCard hash={e.contractHash} name={e.contractName} onClick={() => navigate("/marketplace/" + e.contractHash)}></MarketplaceCard>
+            {marketplaces.map((e: Marketplace, index: number) => (
+              <MarketplaceCard key={index} hash={e.contractHash} name={e.contractName} onClick={() => navigate("/marketplace/" + e.contractHash)}></MarketplaceCard>
             ))}
           </div>
         )}
