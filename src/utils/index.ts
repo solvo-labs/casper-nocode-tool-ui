e4282c494ea8d13907aa5448c66ee27776da9309
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-11-17 17:27:26 +0300

    mergable nft contract address added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -70,6 +70,8 @@ export const sleep = (ms: number) => {
 
 export const STORE_RAFFLE_CONTRACT_HASH = "6cbf0ee026d1d6ebc0364308213ce859895278f73bb15744b5089f8335adb8c8";
 export const lootboxStorageContract = "0e99476bb8b2cdade2f916bfaad7f844120bd0bb1d3ab170d7ab501b4fa850ba";
+export const MERGABLE_NFT_CONTRACT = "9c05cd692df83244424dbd0a5b5758b72967155097ac37402c8fbd5685ceeb9a";
+
 const rarityLevelExplanationTitleArray: string[] = [
   "COMMON: This level is given to the most frequently encountered items inside loot boxes. These items are generally common and widely found.",
   "RARE: This level is given to less common and more special items. These items are rarer within the general population and possess special value.",
